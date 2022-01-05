a2a8bc3f05c6933e1178d41bef67659db17a23cd
Niqujoe <nikie.deocampo@gmail.com>
2022-01-05 22:48:32 +0800

    Add link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="#"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
