4d6131dad981d683f20c98f20b1f1c9083cc99ae
Niqujoe <nikie.deocampo@gmail.com>
2022-01-05 22:36:39 +0800

    Add code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to my site
+          <code>Hello</code> Welcome to my site!
         </p>
         <a
           className="App-link"
