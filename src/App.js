5df4970be778d3accb153755963005bc96c10999
unknown <nikie.deocampo@gmail.com>
2022-01-05 22:23:00 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          THIS IS A TEST
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          DevcrewPH lang malakas
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
